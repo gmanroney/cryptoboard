1c14fab2fcae1a40a90bf8d2d2e87e9abf1ff554
Gerard Moroney <gandc@upraxis.com>
2017-12-29 19:13:15 +0000

    Refined code to make it easier to call function to open websocket and subscribe to multiple channels for #2

--- src/server.js
+++ src/server.js
@@ -11,45 +11,47 @@ client.on('connect', function() {
     console.log('Connected to Redis Server');
 });
 
-// Connect To Exchange
-const WebSocket = require('ws')
-const wss = new WebSocket('wss://api.bitfinex.com/ws/')
+function processOne() {
+  // Connect To Exchange
+  const WebSocket = require('ws');
+  const wss = new WebSocket('wss://api.bitfinex.com/ws/');
 
-// Subscribe To Channel In Exchange
-var bc_queue = 'BITFINEX:BTCUSD';
-wss.onopen = () => {
-  console.log('Subscribing: BTCUSD');
-  wss.send(JSON.stringify(
+  // Subscribe To Channel In Exchange
+  wss.onopen = () => {
+    // Send request to subscribe
+    wss.send(JSON.stringify(
+      {
+        "event": "subscribe",
+        "channel": "trades",
+        "pair": "BTCUSD"
+      }
+    ));
+  }
+
+  // Parse channel information and send to Redis
+  wss.onmessage = (msg) => {
+    var resp = JSON.parse(msg.data);
+    var head = resp["event"];
+    var head_body = resp[1];
+
+    // Get channel and symbol from response
+    if ( head == "subscribed" )
     {
-      "event": "subscribe",
-      "channel": "trades",
-      "pair": "BTCUSD"
-    }
-  ));
-}
-
-// Parse channel information and send to Redis
-wss.onmessage = (msg) => {
-  //console.log(msg.data)
-  var resp = JSON.parse(msg.data);
-  var head = resp["event"];
-  var head_body = resp[1];
-
-  //console.log(head, msg.data)
-  //console.log(hb);
-  if ( head == "subscribed" ) {
-    console.log( bc_queue, " channelID = ", resp["chanId"], " currency = ", resp["pair"]);
-  } else {
-    if ( resp[1] == "tu")
-    {
-      tr_timestamp=new Date(resp[4]*1000);
-      tr_id=resp[3];
-      tr_price=resp[5];
-      tr_amount=resp[6];
-      tr_side=( tr_amount > 0 ? "buy" : "sell" );
-      var msg = { "tr_id": tr_id, "tr_timestamp": tr_timestamp, "tr_price": tr_price, "tr_amount": tr_amount, "tr_side": tr_side };
-      console.log(bc_queue, tr_id);
-      client.publish(bc_queue,JSON.stringify(msg));
+      console.log( bc_queue, " channelID = ", resp["chanId"], " currency = ", resp["pair"]);
+    } else {
+      if ( resp[1] == "tu")
+      {
+        // Transform message and send to Redis channel named exchange:symbol
+        var bc_queue = 'BITFINEX:BTCUSD';
+        tr_timestamp=new Date(resp[4]*1000);
+        tr_id=resp[3];
+        tr_price=resp[5];
+        tr_amount=resp[6];
+        tr_side=( tr_amount > 0 ? "buy" : "sell" );
+        var msg = { "tr_id": tr_id, "tr_timestamp": tr_timestamp, "tr_price": tr_price, "tr_amount": tr_amount, "tr_side": tr_side };
+        console.log(bc_queue, tr_id);
+        client.publish(bc_queue,JSON.stringify(msg));
+      }
     }
   }
 }
@@ -64,6 +66,8 @@ function main () {
     console.log("Exchange Pairs Count = "  + JSON.stringify(config.exchanges[i]["pairs"].length));
     console.log("FUNCTION -> ",config.exchanges[i]["wssurl"], config.exchanges[i]["name"],config.exchanges[i]["pairs"] )
   };
+
+  processOne();
 
 }
 
